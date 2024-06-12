d70d1f02fa4b27867bec99c37d0001814a5ab0b9
jooys130 <wndustn130@gmail.com>
2024-06-12 15:06:04 +0900

    Title: 싸움땅, Time: 120ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240612/싸움땅/battle-ground.java
+++ 240612/싸움땅/battle-ground.java
@@ -51,9 +51,16 @@ public class Main {
         for (int round = 0; round < k; round++) {
             // 1 ~ n번 플레이어 순서대로
             for (int i = 1; i < m+1; i++) {
+                // 기존 위치 없앰
+                Player p = players[i];
+                map[p.x][p.y] = 0;
                 move(i);
                 if (!existPlayer(i)) {
+                    // 총 줍기
                     getGuns(i);
+                    // 갱신
+                    p = players[i];
+                    map[p.x][p.y] = i;
                 } else {
                     fight(i);
                 }
@@ -96,13 +103,19 @@ public class Main {
 
         // 진 플레이어는 총을 해당 격자에 내려두고 원래 가진 방향으로 한 칸 이동
         Player loser = players[L];
-        guns[loser.x][loser.y].add(loser.gun);
-        players[L].gun = 0;
+        // ?????? 총 없을 때ㅔ add가 문제라고 ???????????????
+        if (loser.gun != 0) {
+            guns[loser.x][loser.y].add(loser.gun);
+            players[L].gun = 0;
+        }
         move2(L);
 
         // 이긴 플레이어는 승리한 칸에 있는 총 중 가장 높은 공격력을 가진 총 획득하고
             // 나머지는 그대로 격자에 두기
         getGuns(win);
+        /// 굴러온 애가 이겼을 때
+        Player winner = players[win];
+        map[winner.x][winner.y] = win;
     }
     public static void move2(int num) {
         Player p = players[num];
@@ -119,7 +132,7 @@ public class Main {
                 p.dir = nd;
                 // map[p.x][p.y] = 0; // 원래 잇던 곳
                 p.x = nx; p.y = ny;
-                map[p.x][p.y] = num; // 일하는 곳
+                map[p.x][p.y] = num; // 이동한 곳
                 break;
             }
         }
@@ -144,9 +157,9 @@ public class Main {
             p.dir = nd;
         }
         // player 정보 업데이트
-        map[p.x][p.y] = 0;
+        // map[p.x][p.y] = 0;
         p.x = nx; p.y = ny;
-        // map[p.x][p.y] = num; // 이건 있는지 확인하고 난 후
+        // map[p.x][p.y] = num; // 이건싸우고나서 확인하고 난 후
     }
     public static void getGuns(int k) {
         // 총 있다면 총 획득
@@ -168,7 +181,7 @@ public class Main {
             }
         }
         // 플레이어 정보 업데이트
-        map[p.x][p.y] = k; // (3) 위치 저장
+        // map[p.x][p.y] = k; // (?) 위치 저장
         // System.out.println(map[p.x][p.y]);
     }
     public static boolean existPlayer(int k) {
